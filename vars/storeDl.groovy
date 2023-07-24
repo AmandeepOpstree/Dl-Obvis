0f0a336d0969054a3dc8ad1200ac611245a21a11
AmandeepOpstree <130042733+AmandeepOpstree@users.noreply.github.com>
2023-07-24 15:11:05 +0530

    Update storeDl.groovy

--- vars/storeDl.groovy
+++ vars/storeDl.groovy
@@ -1,4 +1,15 @@
-def call(Map params) {
-    def distributionList = params?.distributionList ?: []
-    return distributionList
+def call(String userEmail) {
+    def distributionList = loadDistributionList()
+    if (!distributionList.contains(userEmail)) {
+        distributionList.add(userEmail)
+        saveDistributionList(distributionList)
+    }
+}
+
+def loadDistributionList() {
+    return env.DISTRIBUTION_LIST ? env.DISTRIBUTION_LIST.split(',') : []
+}
+
+def saveDistributionList(List distributionList) {
+    env.DISTRIBUTION_LIST = distributionList.join(',')
 }
