329c6fcda50823105a40dfaf60747fef1f13a7d2
AmandeepOpstree <130042733+AmandeepOpstree@users.noreply.github.com>
2023-07-24 15:15:04 +0530

    Update storeDl.groovy

--- vars/storeDl.groovy
+++ vars/storeDl.groovy
@@ -3,6 +3,9 @@ def call(String userEmail) {
     if (!distributionList.contains(userEmail)) {
         distributionList.add(userEmail)
         saveDistributionList(distributionList)
+        echo "User ${userEmail} added to the distribution list."
+    } else {
+        echo "User ${userEmail} is already in the distribution list."
     }
 }
 
